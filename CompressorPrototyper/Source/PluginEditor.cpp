67e8ff574bb79231ed8b9f73bcb6d32c8a959a81
Landon <33662627+landonviator@users.noreply.github.com>
2021-01-19 15:53:30 -0600

    Gui mostly done

--- CompressorPrototyper/Source/PluginEditor.cpp
+++ CompressorPrototyper/Source/PluginEditor.cpp
@@ -13,9 +13,69 @@
 CompressorPrototyperAudioProcessorEditor::CompressorPrototyperAudioProcessorEditor (CompressorPrototyperAudioProcessor& p)
     : AudioProcessorEditor (&p), audioProcessor (p)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize (400, 300);
+    int leftMargin = 32;
+    int topMargin = 64;
+    
+    shadowProperties.radius = 15;
+    shadowProperties.offset = juce::Point<int> (-2, 6);
+    dialShadow.setShadowProperties (shadowProperties);
+    
+    sliders.reserve(6);
+    sliders = {
+        &inputSlider, &ratioSlider, &threshSlider, &attackSlider, &releaseSlider, &trimSlider
+    };
+    
+    labels.reserve(6);
+    labels = {
+             &inputLabel, &ratioLabel, &threshLabel, &attackLabel, &releaseLabel, &trimLabel
+        };
+        
+    labelTexts.reserve(6);
+    labelTexts = {
+        inputSliderLabelText, ratioSliderLabelText, threshSliderLabelText, attackSliderLabelText, releaseSliderLabelText, trimSliderLabelText
+    };
+        
+    
+    for (auto i = 0; i < sliders.size(); i++) {
+        addAndMakeVisible(sliders[i]);
+        sliders[i]->setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+        sliders[i]->setTextBoxStyle(juce::Slider::TextBoxBelow, false, 64, 32);
+        sliders[i]->setColour(0x1001400, juce::Colour::fromFloatRGBA(1, 1, 1, 0.5f));
+        sliders[i]->setColour(0x1001700, juce::Colour::fromFloatRGBA(1, 1, 1, 0.0f));
+        sliders[i]->setColour(0x1001500, juce::Colour::fromFloatRGBA(0, 0, 0, 0.25f));
+        sliders[i]->setLookAndFeel(&customDial);
+        sliders[i]->setComponentEffect(&dialShadow);
+        
+        if (sliders[i] == &inputSlider) {
+            sliders[i]->setBounds(leftMargin, topMargin * 1.25, 172, 172);
+        } else {
+            sliders[i]->setBounds(sliders[i - 1]->getX() + sliders[i - 1]->getWidth(), topMargin * 1.25f, 172, 172);
+        }
+    }
+    
+    inputSlider.setRange(-36, 36, 0.5f);
+    ratioSlider.setRange(1, 30, 1);
+    threshSlider.setRange(-96, 0, 0.5f);
+    attackSlider.setRange(1, 1000, 1);
+    releaseSlider.setRange(1, 1000, 1);
+    trimSlider.setRange(-36, 36, 0.5f);
+
+    
+    for (auto i = 0; i < labels.size(); i++) {
+            addAndMakeVisible(labels[i]);
+            labels[i]->setText(labelTexts[i], juce::dontSendNotification);
+            labels[i]->setJustificationType(juce::Justification::centred);
+            labels[i]->setColour(0x1000281, juce::Colour::fromFloatRGBA(1, 1, 1, 0.25f));
+            labels[i]->attachToComponent(sliders[i], false);
+        }
+    
+    addAndMakeVisible(windowBorder);
+    windowBorder.setText("Compressor");
+    windowBorder.setColour(0x1005400, juce::Colour::fromFloatRGBA(1, 1, 1, 0.25f));
+    windowBorder.setColour(0x1005410, juce::Colour::fromFloatRGBA(1, 1, 1, 0.25f));
+    windowBorder.setBounds(16, 16, 1068, 296);
+    
+    setSize (1100, 328);
 }
 
 CompressorPrototyperAudioProcessorEditor::~CompressorPrototyperAudioProcessorEditor()
@@ -25,12 +85,11 @@ CompressorPrototyperAudioProcessorEditor::~CompressorPrototyperAudioProcessorEdi
 //==============================================================================
 void CompressorPrototyperAudioProcessorEditor::paint (juce::Graphics& g)
 {
-    // (Our component is opaque, so we must completely fill the background with a solid colour)
-    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.fillAll (juce::Colour::fromFloatRGBA(0.1f, 0.12f, 0.16f, 1.0));
+        
+    juce::Rectangle<int> background = AudioProcessorEditor::getLocalBounds();
+    g.setGradientFill(juce::ColourGradient::vertical(juce::Colour::fromFloatRGBA(0.18f, 0.20f, 0.24f, 1.0), 300 * .01, juce::Colour::fromFloatRGBA(0.12f, 0.14f, 0.18f, 1.0), 300 * .99));
+    g.fillRect(background);
 }
 
 void CompressorPrototyperAudioProcessorEditor::resized()
--- CompressorPrototyper/Source/PluginEditor.h
+++ CompressorPrototyper/Source/PluginEditor.h
@@ -14,6 +14,33 @@
 //==============================================================================
 /**
 */
+
+class ViatorDial : public juce::LookAndFeel_V4{
+public:
+    void drawRotarySlider(juce::Graphics &g, int x, int y, int width, int height, float sliderPos, float rotaryStartAngle, float rotaryEndAngle, juce::Slider &slider) override{
+        
+        float diameter = fmin(width, height) * 0.65;
+        float radius = diameter * 0.5;
+        float centerX = x + width * 0.5;
+        float centerY = y + height * 0.5;
+        float rx = centerX - radius;
+        float ry = centerY - radius;
+        float angle = rotaryStartAngle + (sliderPos * (rotaryEndAngle - rotaryStartAngle));
+        
+        juce::Rectangle<float> dialArea (rx, ry, diameter, diameter);
+        g.setColour(juce::Colour::fromFloatRGBA(0.15, 0.15, 0.15, 1)); //center
+        g.setGradientFill(juce::ColourGradient::horizontal(juce::Colour::fromFloatRGBA(0.1, 0.1, 0.1, 1), centerY * .25, juce::Colour::fromFloatRGBA(0.15, 0.15, 0.15, 1), centerY * 2.5));
+        g.fillEllipse(dialArea);
+        g.setColour(juce::Colour::fromFloatRGBA(0.392f, 0.584f, 0.929f, 0.5f)); //tick color
+
+        juce::Path dialTick;
+        dialTick.addRectangle(0, -radius + 3, 3.0f, radius * 0.452211);
+        g.fillPath(dialTick, juce::AffineTransform::rotation(angle).translated(centerX, centerY));
+        g.setColour(juce::Colour::fromFloatRGBA(0, 0, 0, .25)); //outline
+        g.drawEllipse(rx, ry, diameter, diameter, 1.0f);
+    }
+};
+
 class CompressorPrototyperAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
@@ -25,8 +52,27 @@ public:
     void resized() override;
 
 private:
-    // This reference is provided as a quick way for your editor to
-    // access the processor object that created it.
+    
+    juce::Slider inputSlider, ratioSlider, threshSlider, attackSlider, releaseSlider, trimSlider;
+    std::vector<juce::Slider*> sliders;
+    
+    juce::GroupComponent windowBorder;
+    
+    juce::Label inputLabel, ratioLabel, threshLabel, attackLabel, releaseLabel, trimLabel;
+    std::vector<juce::Label*> labels;
+    
+    std::string inputSliderLabelText = "Input";
+    std::string ratioSliderLabelText = "Ratio";
+    std::string threshSliderLabelText = "Thresh";
+    std::string attackSliderLabelText = "Attack";
+    std::string releaseSliderLabelText = "Release";
+    std::string trimSliderLabelText = "Trim";
+    std::vector<std::string> labelTexts;
+    
+    ViatorDial customDial;
+    juce::DropShadow shadowProperties;
+    juce::DropShadowEffect dialShadow;
+    
     CompressorPrototyperAudioProcessor& audioProcessor;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (CompressorPrototyperAudioProcessorEditor)
